b746bd82fd14c88938e17cc5149e1c9af170d903
Kyrylo Anastasiev <kyrylo_anastasiev@epam.com>
2026-04-17 16:30:54 +0300

    Add JsonMapper bean

--- src/main/java/com/epam/laboratory/app/config/AppConfig.java
+++ src/main/java/com/epam/laboratory/app/config/AppConfig.java
@@ -1,9 +1,30 @@
 package com.epam.laboratory.app.config;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "com.epam.laboratory.app")
 public class AppConfig {
+
+    @Bean
+    public JsonMapper objectMapper() {
+        return JsonMapper.builder()
+                .findAndAddModules()
+                .configure(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY, false)
+                .configure(MapperFeature.SORT_CREATOR_PROPERTIES_BY_DECLARATION_ORDER, true)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .enable(SerializationFeature.INDENT_OUTPUT)
+                .enable(SerializationFeature.FLUSH_AFTER_WRITE_VALUE)
+                .enable(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS)
+                .enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build();
+    }
 }
