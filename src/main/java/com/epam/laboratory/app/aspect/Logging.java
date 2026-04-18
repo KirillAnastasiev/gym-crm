c4c666c4bc8bdc619a6609652c9ceb0edb63937c
Kyrylo Anastasiev <kyrylo_anastasiev@epam.com>
2026-04-18 08:13:17 +0300

    Add Logging annotation, add LoggingAspect

---
+++ src/main/java/com/epam/laboratory/app/aspect/Logging.java
@@ -0,0 +1,15 @@
+package com.epam.laboratory.app.aspect;
+
+import org.slf4j.event.Level;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Logging {
+    Level value() default Level.TRACE;
+}
+
---
+++ src/main/java/com/epam/laboratory/app/aspect/LoggingAspect.java
@@ -0,0 +1,62 @@
+package com.epam.laboratory.app.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.event.Level;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+    @Before("@annotation(logging)")
+    public void logMethodEntry(JoinPoint joinPoint, Logging logging) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        Object[] args = joinPoint.getArgs();
+
+        String logMessage = String.format("Entering method: %s.%s with arguments: %s",
+                className, methodName, Arrays.toString(args));
+
+        logByLevel(logging.value(), logMessage);
+    }
+
+    @AfterReturning(pointcut = "@annotation(logging)", returning = "result")
+    public void logMethodExit(JoinPoint joinPoint, Logging logging, Object result) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+
+        String logMessage = String.format("Exiting method: %s.%s with result: %s", className, methodName, result);
+
+        logByLevel(logging.value(), logMessage);
+    }
+
+    @AfterThrowing(pointcut = "@annotation(logging)", throwing = "exception")
+    public void logMethodException(JoinPoint joinPoint, Logging logging, Exception exception) {
+        String methodName = joinPoint.getSignature().getName();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+
+        String logMessage = String.format("Exception in method: %s.%s with message: %s",
+                className, methodName, exception.getMessage());
+
+        log.error(logMessage, exception);
+    }
+
+    private void logByLevel(Level level, String message) {
+        switch (level) {
+            case DEBUG -> log.debug(message);
+            case INFO -> log.info(message);
+            case WARN -> log.warn(message);
+            case ERROR -> log.error(message);
+            default -> log.trace(message);
+        }
+    }
+}
+
+
